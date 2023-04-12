2761782859aacac7abb6c2aec813e7297e7424a1
Diogo Rocha <34406363+diogorocha18@users.noreply.github.com>
2023-04-12 07:57:00 +0000

    test script

--- Buttons-Information-Village/general.js
+++ Buttons-Information-Village/general.js
@@ -1,1 +1,12 @@
+// ==UserScript==
+// @name          Buttons/Information in the Village
+// @author        Diogo Rocha
+// @version       Release
+// @icon          https://i.imgur.com/a84lbDO.png
+// @include       https://*tribalwars.com.pt*village*overvie*
+// @grant         none
+// @downloadURL   https://raw.githubusercontent.com/diogorocha18/ScriptsPublicos/main/Buttons-Information-Village/general.js
+// @updateURL     https://raw.githubusercontent.com/diogorocha18/ScriptsPublicos/main/Buttons-Information-Village/general.js
+// ==/UserScript==
+
 console.log("test")

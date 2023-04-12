653d9366f38cca05bfd59e7050e767d49c8d0bea
Diogo Rocha <34406363+diogorocha18@users.noreply.github.com>
2023-04-12 08:36:34 +0000

    update script final

--- Buttons-Information-Village/loader.js
+++ Buttons-Information-Village/loader.js
@@ -1,13 +1,6 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://raw.githubusercontent.com/diogorocha18/ScriptsPublicos/main/Buttons-Information-Village/general.js");
-xhr.onload = function() {
-	if (xhr.status === 200) {
-		eval(xhr.responseText);
-	} else {
-		console.error(xhr.statusText);
-	}
-};
-xhr.onerror = function() {
-	console.error("Erro ao obter o script.");
-};
-xhr.send();
+$.ajax({
+	type: "GET",
+	url: 'https://raw.githubusercontent.com/diogorocha18/ScriptsPublicos/main/Buttons-Information-Village/general.js',
+	dataType: "script",
+	cache: false
+});

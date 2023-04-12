720ce9b43853015d83231df1529570b4ee199b48
Diogo Rocha <34406363+diogorocha18@users.noreply.github.com>
2023-04-12 23:27:42 +0100

    Update loader.js

--- Buttons-Information-Village/loader.js
+++ Buttons-Information-Village/loader.js
@@ -1,10 +1,6 @@
-if (window.location.href.includes('&mode=units')) {
-  console.log("Não é possível executar o script nesta página.");
-} else {
-	$.ajax({
-		type: "GET",
-		url: 'https://raw.githubusercontent.com/diogorocha18/Scripts/main/Buttons-Information-Village/general.js',
-		dataType: "script",
-		cache: false
-	});
-}
+$.ajax({
+    type: "GET",
+    url: 'https://raw.githubusercontent.com/diogorocha18/Scripts/main/Buttons-Information-Village/general.js',
+    dataType: "script",
+    cache: false
+});
